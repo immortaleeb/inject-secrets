bb3a30701753a564cbab68ad5f344ee85e2052dd
immortaleeb <1183783+immortaleeb@users.noreply.github.com>
2024-03-07 12:17:36 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    #flake-utils.lib.eachDefaultSystem (system:
+      let
+        system = "aarch64-darwin";
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python312;
+        pythonPackages = pkgs.python312Packages;
+        dependencies = with pythonPackages; [ pip build setuptools ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zsh
+            python
+          ] + dependencies;
+        };
+        defaultPackage.${system} = pythonPackages.buildPythonPackage {
+          pname = "inject-secrets";
+          src = ./.;
+          version = "0.0.1";
+          format = "pyproject";
+          propagatedBuildInputs = dependencies;
+        };
+      };
+    #);
+}
